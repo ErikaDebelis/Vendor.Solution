1a4196b348ff56fca542c5d1a06644d3c17b7758
Erika Debelis <erika.debelis@gmail.com>
2021-07-23 10:56:46 -0700

    2nd vendor test- set vendor id;add logic to id in vendor.cs-PASS

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -9,13 +9,15 @@ namespace VendorAndOrderTracker.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
-    
+
     public List<Order> Orders { get; set; }
 
     public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
       Description = vendorDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
     public static void ClearAll()
     {
